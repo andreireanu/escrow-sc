e080c833f5ae406c6d2270a5828d87921ef871a1
Andrei Ungureanu <andrei.reanu@gmail.com>
2024-12-14 19:30:49 +0200

    Wrap up contract

--- src/escrow_sc.rs
+++ src/escrow_sc.rs
@@ -13,6 +13,28 @@ pub struct Offer<M: ManagedTypeApi> {
     pub accepted_address: ManagedAddress<M>,
 }
 
+impl<M: ManagedTypeApi> Offer<M> {
+    pub fn new(
+        creator: ManagedAddress<M>,
+        offered_payment: EsdtTokenPayment<M>,
+        accepted_token: TokenIdentifier<M>,
+        accepted_nonce: u64,
+        accepted_amount: BigUint<M>,
+        accepted_address: ManagedAddress<M>,
+    ) -> Self {
+        Offer {
+            creator,
+            offered_payment,
+            accepted_payment: EsdtTokenPayment::new(
+                accepted_token,
+                accepted_nonce,
+                accepted_amount,
+            ),
+            accepted_address,
+        }
+    }
+}
+
 #[multiversx_sc::contract]
 pub trait EscrowSc {
     #[init]
@@ -33,19 +55,20 @@ pub trait EscrowSc {
         let payment = self.call_value().single_esdt();
         let caller = self.blockchain().get_caller();
         let new_offer_id = self.get_new_offer_id();
+        self.created_offers(&caller).insert(new_offer_id);
+        self.wanted_offers(&accepted_address).insert(new_offer_id);
 
-        let offer = Offer {
-            creator: caller,
-            offered_payment: payment,
-            accepted_payment: EsdtTokenPayment::new(
-                accepted_token,
-                accepted_nonce,
-                accepted_amount,
-            ),
+        let offer = Offer::new(
+            caller,
+            payment,
+            accepted_token,
+            accepted_nonce,
+            accepted_amount,
             accepted_address,
-        };
+        );
 
         self.offers(new_offer_id).set(offer);
+
         new_offer_id
     }
 
@@ -65,6 +88,9 @@ pub trait EscrowSc {
         );
 
         self.offers(offer_id).clear();
+        self.created_offers(&caller).swap_remove(&offer_id);
+        self.wanted_offers(&offer.accepted_address)
+            .swap_remove(&offer_id);
     }
 
     #[payable("*")]
@@ -91,6 +117,9 @@ pub trait EscrowSc {
         );
 
         self.offers(offer_id).clear();
+        self.created_offers(&caller).swap_remove(&offer_id);
+        self.wanted_offers(&offer.accepted_address)
+            .swap_remove(&offer_id);
     }
 
     fn get_offer_by_id(&self, offer_id: OfferId) -> Offer<Self::Api> {
@@ -106,9 +135,46 @@ pub trait EscrowSc {
         new_offer_id
     }
 
+    #[view(getCreatedOffers)]
+    fn get_created_offers(
+        &self,
+        address: ManagedAddress,
+    ) -> MultiValueEncoded<MultiValue2<OfferId, Offer<Self::Api>>> {
+        let mut result = MultiValueEncoded::new();
+        for offer_id in self.created_offers(&address).iter() {
+            result.push(self.get_offer_result(offer_id));
+        }
+        result
+    }
+
+    #[view(getWantedOffers)]
+    fn get_wanted_offers(
+        &self,
+        address: ManagedAddress,
+    ) -> MultiValueEncoded<MultiValue2<OfferId, Offer<Self::Api>>> {
+        let mut result = MultiValueEncoded::new();
+        for offer_id in self.wanted_offers(&address).iter() {
+            result.push(self.get_offer_result(offer_id));
+        }
+        result
+    }
+
+    fn get_offer_result(&self, offer_id: OfferId) -> MultiValue2<OfferId, Offer<Self::Api>> {
+        let offer = self.offers(offer_id).get();
+        MultiValue2::from((offer_id, offer))
+    }
+
+    #[storage_mapper("createdOffers")]
+    fn created_offers(&self, address: &ManagedAddress) -> UnorderedSetMapper<OfferId>;
+
+    #[storage_mapper("wantedOffers")]
+    fn wanted_offers(&self, address: &ManagedAddress) -> UnorderedSetMapper<OfferId>;
+
+    #[view(getOffer)]
     #[storage_mapper("offers")]
     fn offers(&self, id: OfferId) -> SingleValueMapper<Offer<Self::Api>>;
 
+    #[view(getLastOfferId)]
     #[storage_mapper("lastOfferId")]
     fn last_offer_id(&self) -> SingleValueMapper<OfferId>;
 }
